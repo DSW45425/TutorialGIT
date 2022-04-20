0f04b08d209c638dbd07a23d1dba3c27f12cae9b
DSW45425 <104092554+DSW45425@users.noreply.github.com>
2022-04-20 19:00:51 +0200

    Add files via upload

---
+++ tutorial.sh
@@ -0,0 +1,29 @@
+git commit
+git branch
+git checkout
+git checkout -b
+git merge
+git rebase
+git checkout C4
+git checkout C4^
+git branch -f main C6
+git branch -f bugFix C0
+git checkout C1
+git reset local~1
+git checkout pushed
+git revert pushed
+git cherry-pick C3 C4 C7
+git rebase -i main~4 --aboveAll
+git checkout main
+git cherry-pick C4
+git rebase -i caption~2 --aboveAll
+git commit --amend
+git branch -f main caption
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+git tag v0 C1
+git tag 1 C2
+git checkout C2
+git commit
