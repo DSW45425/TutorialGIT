f60bb4a3ceff7aec6983e57cef5af95bc1595fac
DSW45425 <104092554+DSW45425@users.noreply.github.com>
2022-04-20 19:28:44 +0200

    Update tutorial.sh

--- tutorial.sh
+++ tutorial.sh
@@ -1,9 +1,17 @@
 git commit
-git branch
-git checkout
-git checkout -b
-git merge
-git rebase
+git branch bugFix
+git checkout bugFix
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
 git checkout C4
 git checkout C4^
 git branch -f main C6
@@ -13,17 +21,18 @@ git reset local~1
 git checkout pushed
 git revert pushed
 git cherry-pick C3 C4 C7
-git rebase -i main~4 --aboveAll
+git rebase -i master~4 --aboveAll
 git checkout main
 git cherry-pick C4
 git rebase -i caption~2 --aboveAll
 git commit --amend
+git rebase -i caption~2 --aboveAll
 git branch -f main caption
 git checkout main
 git cherry-pick C2
 git commit --amend
 git cherry-pick C3
 git tag v0 C1
-git tag 1 C2
+git tag v1 C2
 git checkout C2
 git commit
